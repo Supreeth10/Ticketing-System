4e46846bd14b122c2e8eae2567445b328a647fde
supreeth10 <smuddu2@pdx.edu>
2025-03-04 17:28:01 -0700

    updated BookingService.java and added new entities and repositories

--- bookingservice/src/main/java/com/example/ticketSystem/bookingservice/service/BookingService.java
+++ bookingservice/src/main/java/com/example/ticketSystem/bookingservice/service/BookingService.java
@@ -1,15 +1,28 @@
 package com.example.ticketSystem.bookingservice.service;
 
+import com.example.ticketSystem.bookingservice.repository.CustomerRepository;
 import com.example.ticketSystem.bookingservice.request.BookingRequest;
 import com.example.ticketSystem.bookingservice.response.BookingResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.example.ticketSystem.bookingservice.entity.Customer;
 
 @Service
 public class BookingService {
 
+    private final CustomerRepository customerRepository;
+
+    @Autowired
+    public BookingService(final CustomerRepository customerRepository){
+        this.customerRepository = customerRepository;
+    }
 
     public BookingResponse createBooking(final BookingRequest request) {
         // check if user exists
+        final Customer customer = customerRepository.findById(request.getUserId()).orElse(null);
+        if(customer ==null){
+            throw new RuntimeException("User not found");
+        }
         // check if there is enough inventory
         // get events info to also get venue info
         // create booking
